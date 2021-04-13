795355ac466ff06c776b7ba4718a7ba6d20126dc
spyridont <spyridon.theodorou@hotmail.com>
2021-04-13 21:48:45 +0200

    toggle btn completed with working filter

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+import { catchError, retry, filter } from 'rxjs/operators';
 
 import { Todo } from '../models/Todo';
 
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 
 import { Todo } from '../models/Todo';
+import { filter } from 'rxjs/operators';
+
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-todo-list',
@@ -14,14 +17,25 @@ import { Todo } from '../models/Todo';
 export class TodoListComponent implements OnInit {
 
   todoList: Todo[];
+  toggleClicked: boolean;
 
   constructor(
     private todoService: TodoService) {
     this.todoList = [];
+    this.toggleClicked = false;
   }
 
   ngOnInit(): void {
     this.getTodos();
+  }
+
+  filterCompleted() {
+    this.toggleClicked = !this.toggleClicked;
+    if (this.toggleClicked) {
+      this.todoList = _.filter(this.todoList, td => td.completed);
+    } else {
+      return this.getTodos();
+    }
   }
 
   getTodos() {
